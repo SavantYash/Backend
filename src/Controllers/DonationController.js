c9091023324285ee92d393ca86a98c093ee80c5f
SavantYash <yashsavant2004@gmail.com>
2025-04-15 12:17:42 +0530

    "cloudinary"

--- src/Controllers/DonationController.js
+++ src/Controllers/DonationController.js
@@ -2,7 +2,6 @@ const multer = require("multer")
 const donationModel = require("../Models/DonationModel")
 const cloudinaryUtil = require("../Utils/CloudanryUtil")
 const path = require("path")
-const fs = require("fs")
 
 const addDonation = async (req, res) => {
     try {
@@ -28,10 +27,10 @@ const getData = async (req, res) => {
     })
 }
 
-const getDataById = async(req,res) => {
-    const data = await donationModel.find({donorId:req.params.id})
+const getDataById = async (req, res) => {
+    const data = await donationModel.find({ donorId: req.params.id })
     res.status(200).json({
-        data : data
+        data: data
     })
 }
 
@@ -49,64 +48,58 @@ const upload = multer({
 }).single("image");
 
 const addDataWithFile = async (req, res) => {
-try{
+
     upload(req, res, async (err) => {
-
         if (err) {
+            console.log(err)
             res.status(500).json({
-                message: err.message,
+                message: "Internal server error",
             });
-        } else {
-            const filepath = path.resolve(req.file.path)
-            const cloundinaryResponse = await cloudinaryUtil.uploadFileToCloudanry(fs.createReadStream(filepath));
-            console.log(cloundinaryResponse);
-            req.body.imageURL = cloundinaryResponse.secure_url
-            const savedDonation = await donationModel.create(req.body);
+        } try {
+            const filepath = path.resolve(req.file.path);
+            const cloudinaryResponse = await cloudinaryUtil.uploadFileToCloudanry(filepath);
+            req.body.imageURL = cloudinaryResponse.secure_url;
 
+            const savedDonation = await donationModel.create(req.body);
             res.status(201).json({
                 message: "donation post successfully",
                 data: savedDonation
             });
+        } catch (error) {
+            console.error("Upload Error:", error);
+            res.status(500).json({ message: "Upload failed", error });
         }
-
     });
-
-}catch(err){
-    res.status(500).json({
-        message:'Internal server error...'
-    })
-    console.log(err)
-}
 }
 
-const updateDonation = async(req,res) => {
-    try{
+const updateDonation = async (req, res) => {
+    try {
         const id = req.params.id
         const data = req.body
-        const updatedData = await donationModel.findByIdAndUpdate(id,data)
+        const updatedData = await donationModel.findByIdAndUpdate(id, data)
         res.status(200).json({
-            "message":"Data updated succesfully...",
-            data : updatedData
+            "message": "Data updated succesfully...",
+            data: updatedData
         })
-    }catch(err){
+    } catch (err) {
         res.status(500).json({
-            "message":"internal server error...",
-            data : err
+            "message": "internal server error...",
+            data: err
         })
     }
 }
 
-const deleteDonation = async(req,res) => {
-    try{
+const deleteDonation = async (req, res) => {
+    try {
         const id = req.params.id
-        const data = await donationModel.findByIdAndDelete({_id:id})
+        const data = await donationModel.findByIdAndDelete({ _id: id })
         res.status(200).json({
-            "message":"Data deleted successfully...",
-            data : data
+            "message": "Data deleted successfully...",
+            data: data
         })
-    }catch(err){
+    } catch (err) {
         res.status(500).json({
-            "message":"Internal server error...",
+            "message": "Internal server error...",
         })
         console.log(err)
     }
